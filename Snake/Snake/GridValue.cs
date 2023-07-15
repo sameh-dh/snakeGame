7fa271444e2f87e49d0219ce722f20db0417c2c6
sameh-dh <samehdh44@gmail.com>
2023-07-15 22:47:33 +0100

    create directions setup

--- Snake/Snake/GridValue.cs
+++ Snake/Snake/GridValue.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace Snake
 {
-    internal class GridValue
+    public enum GridValue
     {
+        Empty,
+        Snake, 
+        Food,
+        Outside
     }
 }
