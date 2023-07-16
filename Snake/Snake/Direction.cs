f0becd027881e9daeb99b43253ba3e05cfcad8ee
sameh-dh <samehdh44@gmail.com>
2023-07-16 14:12:49 +0100

    add snake Position

--- Snake/Snake/Direction.cs
+++ Snake/Snake/Direction.cs
@@ -1,12 +1,50 @@
-﻿using System;
+﻿
+
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Snake
 {
-    internal class Direction
+    public class Direction
     {
+        public readonly static Direction Left = new Direction(0,-1);
+        public readonly static Direction Right = new Direction(0, 1);
+        public readonly static Direction Up = new Direction(-1, 0);
+        public readonly static Direction Down = new Direction(1, 0);
+        public int RowOffset { get; }
+        public int ColumnOffset { get; }
+
+        private Direction (int rowOffset, int colOffset )
+        {
+            RowOffset = rowOffset;
+            ColumnOffset = colOffset;
+        }
+        //method
+        public Direction Opposite()
+        {
+            return new Direction (-RowOffset, -ColumnOffset);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Direction direction &&
+                   RowOffset == direction.RowOffset &&
+                   ColumnOffset == direction.ColumnOffset;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(RowOffset, ColumnOffset);
+        }
+
+        public static bool operator ==(Direction left, Direction right)
+        {
+            return EqualityComparer<Direction>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Direction left, Direction right)
+        {
+            return !(left == right);
+        }
     }
 }
---
+++ Snake/Snake/GameState.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    internal class GameState
+    {
+        public int Rows { get;  }
+        public int  Cols { get;  }
+        public GridValue[,] Grid { get; }
+        public Direction Dir { get; private set; }
+        public bool GameOver { get; private set;  }
+
+
+        private readonly LinkedList<Position> snakePositions = new LinkedList<Position>() ;
+        private readonly Random random = new Random();
+
+        public GameState(int rows, int cols)
+        {
+            Rows = rows;
+            Cols = cols;
+            Grid = new GridValue[rows, cols];
+            Dir = Direction.Right;
+
+            AddSnake();
+        }
+        private void AddSnake()
+        {
+            int r = Rows / 2;
+            for (int c = 1; c <= 3; c++)
+            {
+                Grid[r, c] = GridValue.Snake;
+                snakePositions.AddFirst(new Position(r, c));
+
+            }
+        }
+    }
+}
