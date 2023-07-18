38323d3dcd85de1f5bb16f55c491cb0a3338be88
sameh-dh <samehdh44@gmail.com>
2023-07-18 21:37:06 +0100

    snake game is done

--- Snake/Snake/MainWindow.xaml.cs
+++ Snake/Snake/MainWindow.xaml.cs
@@ -20,9 +20,35 @@ namespace Snake
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly int rows = 15, cols = 15;
+        private readonly Image[,] gridImages; 
+
         public MainWindow()
         {
             InitializeComponent();
+            gridImages = setupGrid();  
+        }
+        //Method 
+        private Image[,] setupGrid()
+        {
+            Image[,] images = new Image[rows, cols];
+            GameGrid.Rows = rows;
+            GameGrid.Columns = cols; 
+
+            for (int r = 0; r<rows; r++)
+            {
+                for (int c=0; c<cols; c++)
+                {
+                    Image image = new Image
+                    {
+                        Source = Images.Empty
+                    };
+
+                    images[r, c] = image;
+                    GameGrid.Children.Add(image);
+                }
+            }
+            return images;
         }
     }
 }
