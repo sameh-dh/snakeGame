2f86225168aa71df4d70cc94daf85ccd6359e9a0
sameh-dh <samehdh44@gmail.com>
2023-07-18 22:03:31 +0100

    visual presentation of the gri snak

--- Snake/Snake/MainWindow.xaml.cs
+++ Snake/Snake/MainWindow.xaml.cs
@@ -20,14 +20,33 @@ namespace Snake
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly Dictionary<GridValue, ImageSource> gridValToImage = new()
+        {
+            {GridValue.Empty, Images.Empty },
+            {GridValue.Snake, Images.Body },
+            {GridValue.Food, Images.Food }
+        };
         private readonly int rows = 15, cols = 15;
-        private readonly Image[,] gridImages; 
-
+        private readonly Image[,] gridImages;
+        private GameState gameState; 
         public MainWindow()
         {
             InitializeComponent();
-            gridImages = setupGrid();  
+            gridImages = setupGrid();
+            gameState = new GameState(rows, cols);
         }
+        private void Window_Loaded (object sender , RoutedEventArgs e)
+        {
+            Draw();
+        }
+        private void Window_KeyDown(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+
         //Method 
         private Image[,] setupGrid()
         {
@@ -49,6 +68,23 @@ namespace Snake
                 }
             }
             return images;
+        }
+
+        private void Draw()
+        {
+            DrawGrid();
+        }
+
+        private void DrawGrid()
+        {
+            for (int r=0; r<rows; r++)
+            {
+               for (int c= 0; c<cols; c++)
+                {
+                   GridValue gridVal = gameState.Grid[r, c];
+                    gridImages[r, c].Source = gridValToImage[gridVal];
+                }
+            }
         }
     }
 }
